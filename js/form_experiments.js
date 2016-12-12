d0c6aa4bd467edbf3621e43e6e3e19b60824c777
Savanah Steigen <savanahsteigen@gmail.com>
2016-12-12 11:50:39 -0500

    Create form experiment 06

--- js/form_experiments.js
+++ js/form_experiments.js
@@ -5,18 +5,18 @@
  */
 
 /**
- * Automatic upload behavior for managed_file fields.
+ * Automatic upload behavior for file fields.
  *
- * Copied from AutoUpload module.
+ * Copied from AutoUpload module. Slightly modified.
  *
  * @see https://www.drupal.org/project/autoupload
  */
 (function ($) {
-  Drupal.behaviors.managedFileAutoUpload = {
+  Drupal.behaviors.fileAutoUpload = {
     attach: function(context, settings) {
-      $('.form-item input.form-submit[value=Upload]', context).hide();
-      $('.form-item input.form-file', context).change(function() {
-        $parent = $(this).closest('.form-item');
+      $('.auto-upload input.form-submit[value=Upload]', context).hide();
+      $('.auto-upload input.form-file', context).change(function() {
+        var $parent = $(this).closest('.fieldset-wrapper');
 
         //setTimeout to allow for validation
         //would prefer an event, but there isn't one
@@ -32,8 +32,6 @@
   Drupal.behaviors.managedFileConfirmRemove = {
     attach: function(context, settings) {
       var myButton = $('.form-item input.form-submit[value=Remove]', context);
-
-      console.log('hello');
 
       if (myButton.length > 0) {
         var myClick = null;
